c8f12b0b8762fffcec8f5616b6167aa286f12777
aemejuobi <govallen@gmail.com>
2019-06-01 18:04:10 -0500

    Made some changes in setting the times

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     var tableBody = $("tbody");
     var minutesAway = 0;
+    
     var config = {
         apiKey: "AIzaSyD8tez5HCZ9uof3_x--ExN2t5lDwRlUz7E",
         authDomain: "train-scheduler-9693d.firebaseapp.com",
@@ -20,16 +21,45 @@ $(document).ready(function(){
             return hour;
         }
         
-        // function timeAway(){
-        //     var date = new Date();
-        //     var minutes = date.getTime();
-        //     console.log(minutes);
-        // }
-        // timeAway();
+        // Set up current date
+        var date = new Date();
+        // Convert date into current hour:minutes format using moment
+        var currentTime = moment(date, "HH:mm").format("HH:mm");
+        // Define a variable and set it equal to currentTime as an array
+        var currentTimeArray = currentTime.split("");
+        // Define an empty string as a placeholder
+        var militaryTimeTime = "";
 
-        tableBody.append("<tr><td>" + childSnapshot.val().name + "</td><td>" + childSnapshot.val().trainDestination + "</td><td>" + childSnapshot.val().freq + "</td><td>" +
-         minutesAway + "</td><td>" + hours() + "</td></tr>");
+        // Create for loop to loop through currentTimeArray
+        for(var i=0; i<currentTimeArray.length; i++){
+            // if the element at currentArrayTime[i] === ":", remove that element
+            if(currentTimeArray[i] === ":"){
+                currentTimeArray.splice(i, 1);
+            }
+            // Set militaryTimeTime equal to currentTimeArray joined as a string
+            militaryTimeTime = currentTimeArray.join("");
+        }
+        
+        console.log(militaryTimeTime);
+        var timeAhead = moment().add(childSnapshot.val().freq, "m");
+        var nextArrival = moment(timeAhead).format("h:mma");
+        console.log(nextArrival);
+        
+        // Append the values to the table
+        tableBody.append(
+            "<tr><td>" + childSnapshot.val().name + 
+            "</td><td>" + childSnapshot.val().trainDestination + 
+            "</td><td>" + childSnapshot.val().time + 
+            "</td><td>" + childSnapshot.val().freq + 
+            "</td><td>" +
+            minutesAway + 
+            "</td><td>" + nextArrival + "</td></tr>"
+            );
+
+         
     });
+    // var minutesAway = currentTime - freq 
+    // var nextArrival = militaryTimeTime + minutesAway
 
 
     // 
@@ -43,8 +73,8 @@ $(document).ready(function(){
         database.ref().push({
             name: trainName,
             trainDestination: destination,
-            freq: parseInt(frequency),
-            time: trainTime
+            time: moment(trainTime, "HH:mm").format("h:mma"),
+            freq: parseInt(frequency)
         });
 
     });
